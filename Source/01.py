cbe0662e5ccd5feb6294cf5a1682965aefb0ceab
DrKoelli <124901300+DrKoelli@users.noreply.github.com>
2023-02-23 16:07:38 +0100

    Checkwin update

--- Source/01.py
+++ Source/01.py
@@ -1,7 +1,27 @@
 """
-A school project
-Designing a popular game
-
+Koell's Branch
+TTT - Game
+->Main Loop
 """
-#First Branch from koelli
+
+def checkwin(playfield, rounds_played, current_player):
+    
+    
+    for y in range(0,3):
+        if((playfield[0][y]==current_player)and(playfield[1][y]==current_player)and(playfield[2][y]==current_player)):
+            return True
+
+    for x in range (0,3):
+        if((playfield[x][0]==current_player) and(playfield[x][1]==current_player)and(playfield[x][2]==current_player)):
+            return True
+
+    # diag. nach rechts
+    if((playfield[0][0]==current_player)and(playfield[1][1]==current_player)and(playfield[2][2]==current_player)):
+        return True
+   
+    # diag. nach links
+    if((playfield[2][0]==current_player)and(playfield[1][1]==current_player)and(playfield[0][2])):
+        return True
+   
+    return False
 
