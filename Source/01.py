ca5b1afc241ddbb76f8784ae632fc594605c0b36
JoensJakobBerzellius <124901141+JoensJakobBerzellius@users.noreply.github.com>
2023-02-23 16:52:26 +0100

    Update 01.py

--- Source/01.py
+++ Source/01.py
@@ -2,3 +2,26 @@
 A school project
 Designing a popular game
 """
+
+def save(name,win):
+    fobj=open("scores.txt", "r+")
+    lines = fobj.readlines()
+
+    # check if name already exists
+    for i, line in enumerate(lines):
+        if line.startswith(name + ","):
+                # Parse the existing score value
+            parts = line.strip().split(", ")
+            existing_win = int(parts[1])
+
+                # Update the score and write the file back
+            updated_win = existing_win + win
+            lines[i] = f"{name}, {updated_win}\n"
+            fobj.seek(0)
+            fobj.truncate()
+            fobj.writelines(lines)
+            print("USER FOUND!")
+            return
+            
+    fobj.write("{}, {}\n".format(name, win))
+    fobj.close()
