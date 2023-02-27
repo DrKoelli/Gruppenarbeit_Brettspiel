5274026bf4025d3020fc33ee06cb2cbb55517ee5
Joshy1808 <124901040+Joshy1808@users.noreply.github.com>
2023-02-27 20:03:29 +0100

    Update 01.py

--- Source/01.py
+++ Source/01.py
@@ -1,5 +1,14 @@
 """
-A school project
-Designing a popular game
-Object oriented TIC-TAC-TOE with GUI
+Client-Teil des Gruppenprojekts (TicTacToe)
+Johannes Sieß
+4AHEL
 """
+
+#import-Statements
+import socket
+
+def Client_con():
+    PORT=61111
+    HOST = input("Host IP eingeben ->")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
