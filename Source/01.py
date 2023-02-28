229ee488445faa18e6927566c0b6c01605ecb8fa
Joshy1808 <124901040+Joshy1808@users.noreply.github.com>
2023-02-28 20:03:14 +0100

    Update 01.py
    Client Teil des Programms wurde soweit wie möglich fertiggestellt.
    Funktionsbezeichnungen wurden mit Luca abgeglichen und einheitlich gestaltet.
    !!!MUSS NOCH AUF FUNKTIONALITÄT GEPRÜFT WERDEN!!!

--- Source/01.py
+++ Source/01.py
@@ -1,14 +1,24 @@
 """
 Client-Teil des Gruppenprojekts (TicTacToe)
 Johannes Sieß
+23.02.2023
 4AHEL
 """
 
 #import-Statements
 import socket
 
-def Client_con():
+def Client_connect():
     PORT=61111
-    HOST = input("Host IP eingeben ->")
+    HOST = input()
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
+
+def Client_send(data):
+    s.sendall(data.encode())
+
+def Client_recive():
+    data = s.recv(1024)
+    if not data:
+        break
+    return data
