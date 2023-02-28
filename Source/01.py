7869bf644b10f2dbfbd65507fb29f2459ad653ad
Luca157 <124901526+Luca157@users.noreply.github.com>
2023-02-28 16:11:59 +0100

    Update 01.py
    New Function and comments

--- Source/01.py
+++ Source/01.py
@@ -4,20 +4,29 @@ Luca
 28.02.23
 '''
 
+#Maybe unfinished / Need to test in main branch :/
+
 import socket
 
 def Hostf():
+    #Port and Host IP
     PORT = 61111
     HOST = input("Host IP eingeben ->")
     #Wait for connection
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((HOST, PORT))
 
+def Connectionasteblish():
+    #need this before you can send anything
+    conn, addr = s.accept()
+
 def Sendmessage(data):
+    #sends input
     data = bytes(data, 'utf-8')
     conn.sendall(data)
 
 
 def Recivemessage():
+    #recives input
     data = conn.recv(1024)
     return data
