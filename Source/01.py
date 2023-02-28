a90fb29597c5411e0913cdfc89019f10d3bc1298
Luca157 <124901526+Luca157@users.noreply.github.com>
2023-02-28 15:55:52 +0100

    Host_Function
    New Host Function + send and recive functions

--- Source/01.py
+++ Source/01.py
@@ -1,5 +1,23 @@
-"""
-A school project
-Designing a popular game
-Object oriented TIC-TAC-TOE with GUI
-"""
+'''
+Small Programm to connect a host to a Server and send messages with
+Luca
+28.02.23
+'''
+
+import socket
+
+def Hostf():
+    PORT = 61111
+    HOST = input("Host IP eingeben ->")
+    #Wait for connection
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+
+def Sendmessage(data):
+    data = bytes(data, 'utf-8')
+    conn.sendall(data)
+
+
+def Recivemessage():
+    data = conn.recv(1024)
+    return data
