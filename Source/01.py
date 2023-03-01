e3171abf4466eb622c14b884a95ef26f778b7ed7
JoensJakobBerzellius <124901141+JoensJakobBerzellius@users.noreply.github.com>
2023-03-01 09:22:06 +0100

    Update 01.py

--- Source/01.py
+++ Source/01.py
@@ -4,24 +4,32 @@ Designing a popular game
 """
 
 def save(name,win):
+    #opening teh save file to read and write
     fobj=open("scores.txt", "r+")
+    #saving the content of the file into a list of strings
     lines = fobj.readlines()
 
-    # check if name already exists
+    # check if the given name already exists
+    #enumerate keeps track of the index of each line -> enumerate(['a', 'b', 'c'])-->(0, 'a'), (1, 'b'), (2, 'c')
     for i, line in enumerate(lines):
         if line.startswith(name + ","):
-                # Parse the existing score value
+                #splitting the line into two parts, the second entry->[1] should be the number of existing wins
             parts = line.strip().split(", ")
+            #saving the number of wins into a variable
             existing_win = int(parts[1])
 
-                # Update the score and write the file back
+                # Updating the score
             updated_win = existing_win + win
+            #writing the file  into the line the name was found in the list named files
             lines[i] = f"{name}, {updated_win}\n"
+            #navigating to the beginning of the file
             fobj.seek(0)
+            #deleting the contents of the file
             fobj.truncate()
+            #rewriting the file with the updated list
             fobj.writelines(lines)
             print("USER FOUND!")
             return
-            
+     #if the name does not exist we create a new entry       
     fobj.write("{}, {}\n".format(name, win))
     fobj.close()
